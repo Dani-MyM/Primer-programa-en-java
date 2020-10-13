1fc742af6e7f04b653bebefd31f82757ddf08915
Daniel Gutierrez Calzada <danigutcal02@gmail.com>
2020-10-13 09:31:04 +0200

    Dice mi lugar de residencia y sistema operativo con el que trabajo

---
+++ Prueba2.java
@@ -0,0 +1,11 @@
+public class Prueba {
+
+	public static void main(String args[]) {
+
+		System.out.println("Daniel Gutierrez Calzada");
+                System.out.println("Leon, 2002");
+                System.out.println("Leon, Windows 10");
+
+	}
+
+}
