a6db305a9e65e0c2bcf6d9fb9e8b2b60fc808501
Daniel Gutierrez Calzada <danigutcal02@gmail.com>
2020-10-13 09:27:56 +0200

    Dice mi lugar de nacimiento y año

---
+++ Prueba1.java
@@ -0,0 +1,10 @@
+public class Prueba {
+
+	public static void main(String args[]) {
+
+		System.out.println("Daniel Gutierrez Calzada");
+                System.out.println("Leon, 2002");
+
+	}
+
+}
